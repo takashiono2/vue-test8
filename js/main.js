6f4293b7e220fb599ffa7980e3b70da5d579bfd6
takashiono2 <takashiono710@gmail.com>
2021-07-27 19:12:19 +0900

    emitを使って子から親へ

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-// #リストデータを子に渡す
+// #$emitを使って子から親へ
 (function(){
   'use stirct';
 
-  Vue.component('blog-post',{
-    template: '<div><h2>{{post.title}}</h2><p>{{post.content}}</p></div>',
-    props: ['post']
-  })
+  Vue.component('emit-event',{
+    template: '<button @click="clickEvent">ボタン</button>',
+    methods: {
+      clickEvent: function(){
+        this.$emit('from-child');
+      }
+    }
+  });
   
   var app = new Vue({
     el: '#app',
-    data: {
-      posts: [
-        {'id':0, 'title': 'vue.jsの基礎', 'content': 'about vue.js...'},
-        {'id':1, 'title': 'componentの基礎', 'content': 'about component...'},
-        {'id':2, 'title': 'Vue CLIの基礎', 'content': 'about Vue CLIの基礎...'},
-      ]
+    methods:{
+      alertMessage: function(){
+        alert('子からイベント受け取ったよ');
+      }
     }
-  })
+  });
 })();
+
+// #リストデータを子に渡す
+// (function(){
+//   'use stirct';
+
+//   Vue.component('blog-post',{
+//     template: '<div><h2>{{post.title}}</h2><p>{{post.content}}</p></div>',
+//     props: ['post']
+//   })
+  
+//   var app = new Vue({
+//     el: '#app',
+//     data: {
+//       posts: [
+//         {'id':0, 'title': 'vue.jsの基礎', 'content': 'about vue.js...'},
+//         {'id':1, 'title': 'componentの基礎', 'content': 'about component...'},
+//         {'id':2, 'title': 'Vue CLIの基礎', 'content': 'about Vue CLIの基礎...'},
+//       ]
+//     }
+//   })
+// })();
 
 // v-bindを使うことで親コンポーネントのdataを子コンポーネントに渡す
 // (function(){
